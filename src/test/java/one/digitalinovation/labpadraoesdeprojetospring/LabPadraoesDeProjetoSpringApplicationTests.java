0b9253de288dfc258d25af9c483b11cbeb2a8e31
DolannPlss <marcos.thadeu2010@hotmail.com>
2022-03-09 17:13:04 -0300

    primeiro commit

---
+++ src/test/java/one/digitalinovation/labpadraoesdeprojetospring/LabPadraoesDeProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.labpadraoesdeprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadraoesDeProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
